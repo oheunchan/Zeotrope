201fbda799e244812bf6e05349716cd5ace86356
eunchan <43670513+oheunchan@users.noreply.github.com>
2023-02-19 23:08:44 +0800

    Add ZDefine.h
    Add ZDefine.h

---
+++ zDefine.h
@@ -0,0 +1,35 @@
+#ifndef __Define_H_
+#define __Define_H_
+
+
+#define RedPin    5
+#define GreenPin  6
+#define BluePin   7
+
+//Bluetooth
+#define BT_RXD    4
+#define BT_TXD    2
+
+#define IRPIN     13
+
+#define CHMinus 0xFFA25D
+#define CH      0xFF629D
+#define CHPlus  0xFFE21D
+#define OFF     0xFF22DD
+
+//Motor 
+#define STEPS 10
+#define RPM 15
+#define IN1 11 // IN1
+#define IN2 10 // IN2
+#define IN3 9 // IN3
+#define IN4 8 // IN4
+
+#define DebugMode 1
+
+
+
+
+
+
+#endif
