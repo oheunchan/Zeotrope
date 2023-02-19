1c7a343de748b4b0918899d6286a1bdaf43e3c41
eunchan <43670513+oheunchan@users.noreply.github.com>
2023-02-19 23:08:30 +0800

    Add Include.h
    Add Include.h

---
+++ zInclude.h
@@ -0,0 +1,20 @@
+#ifndef __Include_H_
+#define __Include_H_
+
+
+#include <stdio.h>
+#include <stdarg.h>
+#include "string.h"
+//#include <IRremote.h> //v3.9.0
+//#include <IRremoteInt.h>  //v3.9.0
+//#include <SoftwareSerial.h>
+//#include <Stepper.h>
+#include "IRLed.h"
+#include "Bluetooth.h"
+#include "Motor.h"
+#include "zDefine.h"
+
+
+
+
+#endif
