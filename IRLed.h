6ff49cfb94d595f6aace7ff699a28137f5383bd9
eunchan <43670513+oheunchan@users.noreply.github.com>
2023-03-05 01:48:11 +0900

    Update IRLed.h
    2023.03.05
    IR 프로토콜 추가

--- IRLed.h
+++ IRLed.h
@@ -2,6 +2,13 @@
 #define __IRLed_H_
 
 
+#define IR_LED_ON     0xFFA25D
+#define IR_LED_OFF    0xFF629D
+#define IR_MOTOR_FOR  0xFFE21D
+#define IR_MOTOR_BACK 0xFF22DD
+
+
+
 
 void _printf(const char *s, ...);
 void IR_Init();
