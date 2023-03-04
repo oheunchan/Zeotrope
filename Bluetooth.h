f54f715c2c66c0a54ac70483739ca00b54b580f9
eunchan <43670513+oheunchan@users.noreply.github.com>
2023-03-05 01:47:35 +0900

    Update Bluetooth.h
    2023.03.05
    BT 프로토콜 추가

--- Bluetooth.h
+++ Bluetooth.h
@@ -5,13 +5,19 @@ void _printf(const char *s, ...);
 void BT_Init();
 void Bluetooth();
 
+
 #define BT_PHeader  0xFF
 
 #define LED_ON      0xA2
 #define LED_OFF     0x62
 
-#define MT_FORWARD    0x02
+#define MT_FORWARD    0xE2
 #define MT_BACKWARD   0x22
+
+#define MT_SPD_UP     0x02
+#define MT_SPD_DN     0xC2
+#define MT_POWER_OFF  0xE0
+
 
 typedef enum
 {
