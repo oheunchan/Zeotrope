56203d2805f96925b46b6e978bb86fee7b98796a
eunchan <43670513+oheunchan@users.noreply.github.com>
2023-05-06 21:57:56 +0900

    Update Motor.h
    2023.05.06c
    모터 가감속 및 LED 동작 제어

--- Motor.h
+++ Motor.h
@@ -1,13 +1,22 @@
 #ifndef __MOTOR_H_
 #define __MOTOR_H_
 
+enum{
+	Motor_IDLE=0x00,
+	Motor_1Step_SPD,
+	Motor_2Step_SPD,
+	Motor_3Step_SPD,
+ 	Motor_Max_SPD,
+};
 
 
 void _printf(const char *s, ...);
 void Motor_init();
 void Motor_ON();
 void Motor_OFF();
-void Motor_SPD_DN(int _spd);
-void Motor_SPD_UP(int _spd);
+void Motor_SPD_DN(int spd);
+void Motor_SPD_UP(int spd);
+
+void Motor_Exec();
 
 #endif
