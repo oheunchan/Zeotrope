ba59aa64abfa974e41ae4ffb00cf2ec34be76e21
eunchan <43670513+oheunchan@users.noreply.github.com>
2023-05-06 21:57:53 +0900

    Update Motor.cpp
    2023.05.06c
    모터 가감속 및 LED 동작 제어

--- Motor.cpp
+++ Motor.cpp
@@ -4,7 +4,7 @@
 #include "zDefine.h"
 
 
-
+extern char Motor_Flag;
 
 
 void Motor_init()
@@ -27,26 +27,78 @@ void Motor_init()
 
 void Motor_ON()
 {
-  digitalWrite(MOTOR_EN, LOW);
-  digitalWrite(MOTOR_BRK, LOW);
+	digitalWrite(MOTOR_EN, LOW);
+	digitalWrite(MOTOR_BRK, LOW);
+
+	digitalWrite(RedPin, HIGH);
+	digitalWrite(GreenPin, LOW);
+	digitalWrite(BluePin, LOW);
+	
+	Motor_Flag=ON;
 }
 
 void Motor_OFF()
 {
-  digitalWrite(MOTOR_EN, HIGH);
-  digitalWrite(MOTOR_BRK, HIGH);
+	digitalWrite(MOTOR_EN, HIGH);
+	digitalWrite(MOTOR_BRK, HIGH);
+	Motor_Flag=OFF;
 }
 
-void Motor_SPD_DN(int _spd)
+void Motor_SPD_DN(int spd)
 {
- 
-
-   //return 1;
+ 	if(OCR0B<=0)
+ 	{
+ 		spd=0;
+		OCR0B-=spd;
+ 	}
+	else
+		OCR0B-=spd;
+	_printf("[%d]\r\n",OCR0B);	
 }
 
-void Motor_SPD_UP(int _spd)
+void Motor_SPD_UP(int spd)
 {
+	if(OCR0B>=240)
+	{
+		spd=240;
+		OCR0B=spd;
+	}
+	else{OCR0B+=spd;}
+		
+	_printf("[%d]\r\n",OCR0B);
+}
 
 
-   //return 1;
+void Motor_Exec()
+{
+	static char step=0;
+
+	if(Motor_Flag)
+	{
+		switch(step)
+		{
+			case Motor_IDLE:		step=Motor_1Step_SPD;								break;
+			case Motor_1Step_SPD:	Motor_ON();	OCR0B=50;	step=Motor_2Step_SPD;		break;
+			case Motor_2Step_SPD:	OCR0B=100;				step=Motor_3Step_SPD;		break;
+			case Motor_3Step_SPD:	OCR0B=200;				step=Motor_Max_SPD;			break;
+			case Motor_Max_SPD:		OCR0B=250;				step=Motor_Max_SPD;			break;
+			default : break;
+
+		}
+	}
+	else
+	{
+		switch(step)
+		{
+			case Motor_IDLE:		Motor_OFF();OCR0B=0;	step=Motor_IDLE;			break;
+			case Motor_1Step_SPD:	OCR0B=50;				step=Motor_IDLE;			break;
+			case Motor_2Step_SPD:	OCR0B=100;				step=Motor_1Step_SPD;		break;
+			case Motor_3Step_SPD:	OCR0B=200;				step=Motor_2Step_SPD;		break;
+			case Motor_Max_SPD:		OCR0B=250;				step=Motor_3Step_SPD;		break;
+			default : break;
+
+		}
+	}
+	_printf("speed :[%d] \r\n",OCR0B);
+
 }
