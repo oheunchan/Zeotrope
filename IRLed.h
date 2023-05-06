63310051229312f3c507e8691fd6114a69f14ddf
eunchan <43670513+oheunchan@users.noreply.github.com>
2023-05-06 18:35:48 +0900

    Update IRLed.h
    2023.05.06
    Code 정리 및 Release 모터 구동 및 PWM 제어

--- IRLed.h
+++ IRLed.h
@@ -2,11 +2,14 @@
 #define __IRLed_H_
 
 
-#define IR_LED_ON     0xFFA25D
-#define IR_LED_OFF    0xFF629D
-#define IR_MOTOR_FOR  0xFFE21D
-#define IR_MOTOR_BACK 0xFF22DD
-
+#define IR_LED_ON       0xFFA25D
+#define IR_LED_OFF      0xFF629D
+#define IR_MOTOR_FOR    0xFFE21D
+#define IR_MOTOR_BACK   0xFF22DD
+#define IR_MOTOR_ON     0xFF02FD
+#define IR_MOTOR_OFF    0xFFC23D
+#define IR_MOTOR_SPDDN  0xFFE01F
+#define IR_MOTOR_SPDUP  0xFFA857
 
 
 
