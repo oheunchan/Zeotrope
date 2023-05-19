9a22dc62a62cf1af9b098599d3f95d991f65d96a
eunchan <43670513+oheunchan@users.noreply.github.com>
2023-05-20 01:41:49 +0900

    Update Bluetooth.cpp
    2023.05.20
    BT Protocol 수정
    코드 정리
    Release 버전 1.1

--- Bluetooth.cpp
+++ Bluetooth.cpp
@@ -1,18 +1,20 @@
 #include <SoftwareSerial.h>
+#include "Arduino.h"
 #include "Bluetooth.h"
 #include "Motor.h"
 #include "RGB_Led.h"
 #include "zDefine.h"
-
+#include "System.h"
 
 SoftwareSerial bluetooth(BT_RXD, BT_TXD);
 
 
-extern char LED_Flag;
 
 const int BUFFER_SIZE = 255;
 unsigned char BT_RX_Buf[BUFFER_SIZE];
-char Motor_spd;
+extern char Motor_Flag;
+extern int led_en;
+extern int motor_mode;
 
 
 void BT_Init()
@@ -38,32 +40,29 @@ void Bluetooth()
     for(int i = 0; i < rlen; i++)
     { 
       bt_rx[i]=BT_RX_Buf[i];
-      _printf("len:%d %d data:%x\r\n",rlen,i,bt_rx[i]);
+//      _printf("len:%d %d data:%x\r\n",rlen,i,bt_rx[i]);
     }
-    _printf("chksum: %x\r\n",Chksum);
+//    _printf("chksum: %x\r\n",Chksum);
     if(bt_rx[BT_HEADER]==BT_PHeader){  bt_protocol_flag=1;  Chksum=bt_rx[BT_CMD1]+bt_rx[BT_CMD2]; } //Header Check
  
     if(bt_protocol_flag)
     {
         switch(bt_rx[BT_CMD1])
         {
-          case LED_ON:
-            if(bt_rx[BT_CMD2]==0x5D && bt_rx[BT_CHKSUM]==Chksum)  setColor(ON); LED_Flag=1;  _printf("LED ON");  break;
-          case LED_OFF:
-            if(bt_rx[BT_CMD2]==0x9D && bt_rx[BT_CHKSUM]==Chksum)  setColor(OFF); LED_Flag=0; _printf("LED OFF");   break;
-          case MT_FORWARD: 
-            if(bt_rx[BT_CMD2]==0x1D && bt_rx[BT_CHKSUM]==Chksum)  _printf("MOTOR FORWARD");  break;
-          case MT_BACKWARD:
-            if(bt_rx[BT_CMD2]==0xDD && bt_rx[BT_CHKSUM]==Chksum) _printf("MOTOR BACKWARD");  break;
-          case MT_SPD_UP:
-            if(bt_rx[BT_CMD2]==0xFD && bt_rx[BT_CHKSUM]==Chksum)  
-              _printf("MOTOR Speed UP");  break;
-          case MT_SPD_DN:
-            if(bt_rx[BT_CMD2]==0x3D && bt_rx[BT_CHKSUM]==Chksum)
-               _printf("MOTOR Speed Down");  break;
-          case MT_POWER_OFF:
-            if(bt_rx[BT_CMD2]==0x57 && bt_rx[BT_CHKSUM]==Chksum)      _printf("MOTOR OFF");   break;
-
+     
+			case LED_ON:			led_en=1;						_printf("LED ON\r\n");				break;
+			case LED_OFF:			led_en=0;						_printf("LED OFF\r\n");				break;
+			
+			case MT_FORWARD:		digitalWrite(MOTOR_DIR, LOW); 	_printf("MOTOR FORWARD\r\n");		break;
+			case MT_BACKWARD:		digitalWrite(MOTOR_DIR, HIGH); 	_printf("MOTOR BACKWARD\r\n");		break;
+			case MOTOR_ON:			Motor_Flag=ON; 					_printf("MOTOR ON\r\n");  			break;
+			case MOTOR_OFF:			Motor_Flag=OFF;  				_printf("MOTOR OFF\r\n"); 			break;
+			case MT_NORMAL_MODE:	motor_mode=Motor_Mode_Normal;	_printf("MOTOR NORMAL Mode\r\n"); 	break;
+			case MT_TB_MODE:		motor_mode=Motor_Mode_TB;		_printf("MOTOR Table Mode\r\n");	break;
+			
+			case SOFT_RESET:		RestFunc();															break;
+			case ETC_CMD:											_printf("ETC COMMAND\r\n"); 		break;
+			
           default: break;
         }
     }
