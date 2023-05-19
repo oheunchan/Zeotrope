ff7de22667e2f1b820e295686e4755a09167ecb5
eunchan <43670513+oheunchan@users.noreply.github.com>
2023-05-20 01:41:59 +0900

    Update IRLed.cpp
    2023.05.20
    BT Protocol 수정
    코드 정리
    Release 버전 1.1

--- IRLed.cpp
+++ IRLed.cpp
@@ -5,12 +5,13 @@
 #include "Motor.h"
 #include "RGB_Led.h"
 #include "zDefine.h"
+#include "System.h"
 
 
 IRrecv ir(IRPIN);
 decode_results res;
 
-extern char LED_Flag;
+
 extern char Motor_Flag;
 extern int led_en;
 extern int motor_mode;
@@ -30,13 +31,6 @@ void IR_Init()
 {
   ir.begin(IRPIN);
 }
-void RestFunc()
-{
-	_printf("SOFT RESET\r\n"); 
-  digitalWrite(RESET_PIN,LOW);
-  delay(100);
-  digitalWrite(RESET_PIN,HIGH);
-}
 
 
 void IR_Test()
@@ -49,14 +43,16 @@ void IR_Test()
 
 		case IR_LED_ON:		led_en=1;						_printf("LED ON\r\n");				break;
 		case IR_LED_OFF:	led_en=0;						_printf("LED OFF\r\n");				break;
+
 		case IR_MOTOR_FOR:  digitalWrite(MOTOR_DIR, LOW); 	_printf("MOTOR FORWARD\r\n"); 	 	break;
 		case IR_MOTOR_BACK: digitalWrite(MOTOR_DIR, HIGH); 	_printf("MOTOR BACKWARD\r\n"); 	 	break;
 		case IR_MOTOR_ON:   Motor_Flag=ON; 					_printf("MOTOR ON\r\n");  			break;
 		case IR_MOTOR_OFF:  Motor_Flag=OFF;  				_printf("MOTOR OFF\r\n"); 			break;
 		case IR_MT_NORMAL:	motor_mode=Motor_Mode_Normal;	_printf("MOTOR NORMAL Mode\r\n"); 	break;
 		case IR_MT_TABLE:	motor_mode=Motor_Mode_TB;		_printf("MOTOR Table Mode\r\n");	break;
-		case IR_ETC_CMD:									_printf("ETC COMMAND\r\n"); 		break;
+
 		case IR_SOFR_RESET:	RestFunc();															break;
+		case IR_ETC_CMD:									_printf("ETC COMMAND\r\n"); 		break;
 			
       default:
       break;
