5344748877744e6117bc6d6d4ec4423cbf4e04ef
eunchan <43670513+oheunchan@users.noreply.github.com>
2023-05-20 01:42:23 +0900

    Update Zeotrope.ino
    2023.05.20
    BT Protocol 수정
    코드 정리
    Release 버전 1.1

--- Zeotrope.ino
+++ Zeotrope.ino
@@ -52,9 +52,7 @@ int timer_motor;
 int debug_timer;
 
 
-
 //Flag variable
-char Led_task;
 char Ir_task;
 char debug_flag;
 char Blue_task;
@@ -68,8 +66,6 @@ char motor_task;
 
 char LED_Flag;
 char Motor_Flag;
-
-char Motor_Speed;
 
 
 ISR(TIMER0_COMPA_vect){
@@ -128,45 +124,6 @@ ISR(TIMER0_COMPA_vect){
   
 }
 
-void _printf(const char *s, ...){
-
-    va_list args;
-
-    va_start(args, s);
-
-    int n = vsnprintf(NULL, 0, s, args);//문자열 길이를 먼저 구함
-
-    char *str = new char[n+1]; //메모리 할당
-
-    vsprintf(str, s, args);//실제 내용을 복사
-
-    va_end(args);
-
-    Serial.print(str);
-
-    delete [] str; //메모리 해제
-
-}
-
-void Timer_Init()
-{
-  //TCCR0A = 0; //TCCR0A initialize
-	TCCR0A |= 0x20;//TCCR0A initialize
-	TCCR0B = 0; //TCCR0B initialize
-	TCNT0 = 0;  //TCNT0 initialize
-	OCR0A= 255;
-	OCR0B=0;  
-	TCCR0B |= (1<<WGM02);
-	TCCR0B |= (1<<CS02) | (0<<CS00);
-	TIMSK0 |= (1<<OCIE0A);
-}
-void Port_init()
-{
-	digitalWrite(RESET_PIN,HIGH);	//RESET PIN
-	pinMode(RESET_PIN,OUTPUT);
-	delay(100);
-	
-}
 
 void setup() {
 	// put your setup code here, to run once:
