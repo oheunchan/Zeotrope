6727420ee3a29e66a45a438593d64a9042c6ec70
eunchan <43670513+oheunchan@users.noreply.github.com>
2023-06-10 23:18:26 +0900

    Update PT_Sensor.cpp
    2023.06.10
    Code 정리 및 Photo Sensor Test
    Motor Table 모드가 기본으로 변경

--- PT_Sensor.cpp
+++ PT_Sensor.cpp
@@ -6,17 +6,12 @@ extern char read_val;
 
 void PT_Sensor_Read(void)
 {
- #if 0
-	int read_val=0;
-  	read_val=digitalRead(PHOTO_SENSOR);
 
-	if(read_val)	digitalWrite(RedPin, HIGH);
-	else		digitalWrite(RedPin, LOW);
-#else
+	#if 0
 	read_val=1;
-//	digitalWrite(RedPin, HIGH);
-//	_printf("pt on\r\n");
-#endif
+	#else
+	read_val=!read_val;
+	#endif
 }
 
 
