fecbbdb6d29a5bfdb65dcf1eaf7c434242a59be3
eunchan <43670513+oheunchan@users.noreply.github.com>
2023-06-25 00:40:30 +0900

    Update RGB_Led.h
    2023.06.25
    Photo Sensor
    Neopixel Test코드 작성

--- RGB_Led.h
+++ RGB_Led.h
@@ -4,5 +4,9 @@
 void _printf(const char *s, ...);
 void setColor(int power_on);
 void RGB_LED_Init();
+void NeoPixel_Init();
+void colorWipe(unsigned int c);
+unsigned int Wheel(unsigned int WheelPos);
+
 
 #endif
