63fef43700f2cb5ecb04ff3fdb14f9bf2178ed58
eunchan <43670513+oheunchan@users.noreply.github.com>
2023-06-25 00:40:17 +0900

    Update PT_Sensor.cpp
    2023.06.25
    Photo Sensor
    Neopixel Test코드 작성

--- PT_Sensor.cpp
+++ PT_Sensor.cpp
@@ -2,16 +2,13 @@
 #include "zDefine.h"
 #include "PT_Sensor.h"
 
-extern char read_val;
-
 void PT_Sensor_Read(void)
 {
 
-	#if 0
-	read_val=1;
-	#else
-	read_val=!read_val;
-	#endif
+	if(digitalRead(PHOTO_SENSOR))
+		digitalWrite(STROBE_LED,LOW);
+	else
+		digitalWrite(STROBE_LED,HIGH);
 }
 
 
